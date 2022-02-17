9497565e18a737ff085d8dae8b3037fcdfd6d9f8
mdrayhanislam <96991135+mdrayhanislam@users.noreply.github.com>
2022-02-17 12:03:07 +0600

    Expenses total

--- js/calculator.js
+++ js/calculator.js
@@ -2,9 +2,22 @@
 document.getElementById('calculator-button').addEventListener
 ('click', function(){
     // get amount deposited
-    const depositInput= document.getElementById('Food-input');
-    const depositAmount = depositInput.value ;
-    const depositTotal = document.getElementById('total-expenses');
-    depositTotal.innerText= depositAmount;
-    depositInput.value='';
+    //food
+    const foodInput= document.getElementById('Food-input');
+    const foodAmount = foodInput.value ;
+    foodInput.value='';
+   //Rent
+   const  rentInput= document.getElementById('Rent-input');
+    const rentAmount = rentInput.value ;
+    rentInput.value='';
+    //Clothes
+    const clothesInput= document.getElementById('Clothes-input');
+    const clothesAmount = clothesInput.value ;
+    clothesInput.value='';
+    //total expenses
+    const totalexpenses = parseFloat (foodAmount) + parseFloat (rentAmount) + parseFloat (clothesAmount) ;
+    //Expenses
+    const expensesTotal = document.getElementById('total-expenses');
+    expensesTotal.innerText= totalexpenses;
+   
 })
